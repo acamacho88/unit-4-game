d063c57e817239f9eca8cbca309be6a4b8a6b944
Alex Camacho <alex.c@WalkMe.local>
2018-06-11 22:08:17 -0700

    Added objects for each character

---
+++ assets/javascript/game.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    var characters = {
+        "Grievous": {
+            "HealthPoints": 120,
+            "AttackPower": 10,
+            "CAPower": 7,
+            "CurrHP": 120,
+            "CurrAPower": 10
+        },
+        "Ackbar": {
+            "HealthPoints": 80,
+            "AttackPower": 8,
+            "CAPower": 3,
+            "CurrHP": 80,
+            "CurrAPower": 8
+        },
+        "BobaFett": {
+            "HealthPoints": 110,
+            "AttackPower": 8,
+            "CAPower": 7,
+            "CurrHP": 110,
+            "CurrAPower": 8
+        },
+        "MaceWindu": {
+            "HealthPoints": 140,
+            "AttackPower": 12,
+            "CAPower": 10,
+            "CurrHP": 140,
+            "CurrAPower": 12
+        }
+    };
+
+    var currCharacter = '';
+
+
+})
